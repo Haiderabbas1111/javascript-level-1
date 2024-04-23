40a6d9756807342c5548c37281fb563e4e1c4816
haider abbas <aj6899766@gmail.com>
2024-04-23 01:10:43 +0100

    my first project

---
+++ script.js
@@ -0,0 +1,17 @@
+//varExample
+var ab = 12;
+console.log(ab);
+
+//letExample
+let abc = 14;
+console.log(abc);
+
+//const Example
+const PI = 3.14;
+console.log(PI);
+
+//Hoisted Example
+x = 10;
+console.log(x);
+var x = 10;
+
